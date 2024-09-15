e10165486f0cfd82dec3a98f13ccc82409554f34
Robert Fiteni <robertfiteni@hotmail.com>
2024-09-15 08:47:38 +0200

    Removed unnecessary template code and added AxGrid logo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axpLogo from './resources/axgrid_logo_transparent_medium.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={axpLogo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to AxGrid, the most unpredictable yet exciting energy trading
+          platform.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
