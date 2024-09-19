72e6e5e11e162a78f7d66953307f201975db2ae6
Robert Fiteni <robertfiteni@hotmail.com>
2024-09-19 23:27:21 +0200

    Added logo.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Tabs, Tab, Typography } from '@mui/material';
+import { Tabs, Tab, Typography, Box, Avatar } from '@mui/material';
 import TradeForm from './TradeForm';
 import TradeTable from './TradeTable';
 import TabPanel from './TabPanel';
+import logo from '../resources/axgrid_logo_transparent_small.png';
 
 function Navigation() {
   const [value, setValue] = useState(0);
@@ -20,7 +21,14 @@ function Navigation() {
 
   return (
     <div>
-      <Typography variant="h4" gutterBottom>
+      <Box display="flex" alignItems="center">
+        <Avatar
+          alt="Platform Logo"
+          src={logo}
+          sx={{ width: 256, height: 67, borderRadius: 2 }}
+        />
+      </Box>
+      <Typography variant="h5" gutterBottom>
         Energy Trading Platform
       </Typography>
 
