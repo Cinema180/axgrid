2c25b11822635bc169e1a5287e1c26bcaa65ca79
Robert Fiteni <robertfiteni@hotmail.com>
2024-09-30 10:09:11 +0200

    Refactor: extracted out the rendering of each information section to the helper file.

--- src/components/TradeDetailDialog.tsx
+++ src/components/TradeDetailDialog.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Divider, Grid, Box } from '@mui/material';
+import { Divider } from '@mui/material';
 import { Subscription } from 'rxjs';
 import formConfig from '../resources/formConfig.json';
 import CustomDialog from './CustomDialog';
-import StatusChip from './StatusChip';
 import { tradeService } from '../services/tradeService';
 import { Trade } from '../types/tradeTypes';
-import getFieldLabel from './TradeDetailDialog.helpers';
+import {
+  EnergySourceSpecificInformationSection,
+  GeneralInformationSection,
+} from './TradeDetailDialog.helpers';
 
 interface TradeDetailDialogProps {
   onClose: () => void;
@@ -50,61 +52,12 @@ function TradeDetailDialog(props: TradeDetailDialogProps) {
 
   return (
     <CustomDialog title="Trade Details" open={open} onClose={onClose}>
-      {/* General Information Section */}
-      <Box sx={{ mt: -1, p: 2, borderRadius: 1, bgcolor: 'background.paper' }}>
-        <Typography variant="h6" gutterBottom>
-          General Information
-        </Typography>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body2">Trade ID</Typography>
-            <Typography variant="body1">{trade.id}</Typography>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="body2">Status</Typography>
-            <StatusChip status={trade.status} />
-          </Grid>
-          {commonFields.map((field) => (
-            <Grid item xs={12} sm={6} key={field.name}>
-              <Typography variant="body2">
-                {getFieldLabel(
-                  field.name,
-                  trade.offeringDetails.energySource,
-                  trade.offeringDetails
-                )}
-              </Typography>
-              <Typography variant="body1">
-                {trade.offeringDetails[field.name]?.toString()}
-              </Typography>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
-
+      <GeneralInformationSection trade={trade} fields={commonFields} />
       <Divider />
-
-      {/* Energy Source-Specific Information */}
-      <Box sx={{ p: 2, borderRadius: 1, bgcolor: 'background.paper' }}>
-        <Typography variant="h6" gutterBottom>
-          Energy Source-Specific Information
-        </Typography>
-        <Grid container spacing={2}>
-          {dynamicFields?.map((field) => (
-            <Grid item xs={12} sm={6} key={field.name}>
-              <Typography variant="body2">
-                {getFieldLabel(
-                  field.name,
-                  trade.offeringDetails.energySource,
-                  trade.offeringDetails
-                )}
-              </Typography>
-              <Typography variant="body1">
-                {trade.offeringDetails[field.name]?.toString()}
-              </Typography>
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      <EnergySourceSpecificInformationSection
+        trade={trade}
+        fields={dynamicFields}
+      />
     </CustomDialog>
   );
 }
